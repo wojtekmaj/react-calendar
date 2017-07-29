a170ce6c15be99b62a559d0388de47198cb0c71c
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-07-30 01:35:14 +0200

    Add sample page

---
+++ sample/Sample.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Calendar from 'react-calendar/src/entry';
+
+import './Sample.less';
+
+export default class Sample extends Component {
+  state = {
+    value: new Date(),
+  }
+
+  onChange = value => this.setState({ value })
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className="Sample">
+        <header>
+          <h1>react-calendar sample page</h1>
+        </header>
+        <div className="Sample__container">
+          <main className="Sample__container__content">
+            <Calendar
+              onChange={this.onChange}
+              showWeekNumbers
+              value={value}
+            />
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<Sample />, document.getElementById('react-container'));
---
+++ sample/webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: [
+    './Sample',
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './index.html' },
+    ]),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+};
