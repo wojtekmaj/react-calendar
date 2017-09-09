0e6d1dddbfa65e58a29b41a8f19b311a03c527fa
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-09-10 01:09:46 +0200

    Fox getISOLocalDate in test suite

--- test/shared/dates.js
+++ test/shared/dates.js
@@ -1,3 +1,7 @@
+import { getYear, getDay } from '../../src/shared/dates';
+
+export const getMonth = date => date.getMonth() + 1;
+
 /**
  * Returns local date in ISO-like format (YYYY-MM-DD).
  */
@@ -11,7 +15,7 @@ export const getISOLocalDate = (value) => {
   }
 
   const year = getYear(value);
-  const month = `0${getMonthIndex(value) + 1}`.slice(-2);
+  const month = `0${getMonth(value)}`.slice(-2);
   const day = `0${getDay(value)}`.slice(-2);
 
   return `${year}-${month}-${day}`;
