ce0d07fa40b7a32424c7ee0d66bbc0e6b25c374d
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-01-26 01:06:18 +0100

    Simplify paths in copy-styles

--- copy-styles.js
+++ copy-styles.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.copyFile('./src/Calendar.less', 'dist/Calendar.less', (error) => {
+fs.copyFile('src/Calendar.less', 'dist/Calendar.less', (error) => {
   if (error) {
     throw error;
   }
@@ -8,7 +8,7 @@ fs.copyFile('./src/Calendar.less', 'dist/Calendar.less', (error) => {
   console.log('Calendar.less copied successfully.');
 });
 
-fs.copyFile('./src/Calendar.css', 'dist/Calendar.css', (error) => {
+fs.copyFile('src/Calendar.css', 'dist/Calendar.css', (error) => {
   if (error) {
     throw error;
   }
