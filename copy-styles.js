1acf9d0d6978422a140f77028f948b4b28ac1791
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-03-15 21:50:59 +0100

    Simplify building styles process

--- copy-styles.js
+++ copy-styles.js
@@ -7,11 +7,3 @@ fs.copyFile('src/Calendar.less', 'dist/Calendar.less', (error) => {
   // eslint-disable-next-line no-console
   console.log('Calendar.less copied successfully.');
 });
-
-fs.copyFile('src/Calendar.css', 'dist/Calendar.css', (error) => {
-  if (error) {
-    throw error;
-  }
-  // eslint-disable-next-line no-console
-  console.log('Calendar.css copied successfully.');
-});
