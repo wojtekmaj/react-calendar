a1737b90c080a701a278372c3222f9f479e56f6e
ritz078 <rkritesh078@gmail.com>
2018-06-13 14:12:01 +0530

    lint fix

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -96,14 +96,14 @@ export const getTileClasses = ({
     classes.push(`${className}--hover`);
   }
 
-  if (!(isEqual(dateRange[0], valueRange[0]) && isEqual(dateRange[1], valueRange[1]))){
+  if (!(isEqual(dateRange[0], valueRange[0]) && isEqual(dateRange[1], valueRange[1]))) {
     if (isEqual(dateRange[0], valueRange[0])) {
-      classes.push(className + '--rangeStart')
+      classes.push(`${className}--rangeStart`);
     } else if (isEqual(dateRange[1], valueRange[1])) {
-      classes.push(className + '--rangeEnd')
+      classes.push(`${className}--rangeEnd`);
     }
   } else {
-    classes.push(className+ '--singleSelect')
+    classes.push(`${className}--singleSelect`);
   }
 
   if (isValueWithinRange(now, dateRange)) {
