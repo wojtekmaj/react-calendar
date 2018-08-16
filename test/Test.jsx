c128df1ae4e12e9e550544804c3a7a040f7561d7
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-08-16 16:05:20 +0200

    Disable checking import/no-unresolved on manual import in Test suite

--- test/Test.jsx
+++ test/Test.jsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
+// eslint-disable-next-line import/no-unresolved
 import Calendar from 'react-calendar/src/entry.nostyle';
+// eslint-disable-next-line import/no-unresolved
 import 'react-calendar/src/Calendar.less';
 
 import DateBonduariesOptions from './DateBonduariesOptions';
