efbdb5311accbb1e0817fce660b32b894ee348ee
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-11-13 14:17:21 +0100

    Add tests for Weekdays component, add abbr

---
+++ src/MonthView/__tests__/Weekdays.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Weekdays from '../Weekdays';
+
+/* eslint-disable comma-dangle */
+
+const weekdaysProps = {
+  activeStartDate: new Date(2018, 0, 1),
+};
+
+describe('Weekdays', () => {
+  it('renders proper weekdays (ISO 8601)', () => {
+    const component = mount(
+      <Weekdays
+        {...weekdaysProps}
+        calendarType="ISO 8601"
+      />
+    );
+
+    const weekdays = component.find('.react-calendar__month-view__weekdays__weekday');
+    const firstWeekday = weekdays.first();
+    const firstWeekdayAbbr = firstWeekday.find('abbr');
+
+    expect(weekdays).toHaveLength(7);
+    expect(firstWeekday.text()).toBe('Mon');
+    expect(firstWeekdayAbbr.prop('aria-label')).toBe('Monday');
+  });
+
+  it('renders proper weekdays (US)', () => {
+    const component = mount(
+      <Weekdays
+        {...weekdaysProps}
+        calendarType="US"
+      />
+    );
+
+    const weekdays = component.find('.react-calendar__month-view__weekdays__weekday');
+    const firstWeekday = weekdays.first();
+    const firstWeekdayAbbr = firstWeekday.find('abbr');
+
+    expect(weekdays).toHaveLength(7);
+    expect(firstWeekday.text()).toBe('Sun');
+    expect(firstWeekdayAbbr.prop('aria-label')).toBe('Sunday');
+  });
+});
