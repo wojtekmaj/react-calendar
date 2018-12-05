396635c19c002b4c306bf6ed11fb62964d87cdf4
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-12-05 12:15:37 +0100

    Avoid multiple calls to tileContent and tileClassName
    Fixes #145

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,54 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import mergeClassNames from 'merge-class-names';
 
 import { tileProps } from './shared/propTypes';
 
-const Tile = ({
-  activeStartDate,
-  children,
-  classes,
-  date,
-  formatAbbr,
-  maxDate,
-  maxDateTransform,
-  minDate,
-  minDateTransform,
-  onClick,
-  onMouseOver,
-  style,
-  tileClassName,
-  tileContent,
-  tileDisabled,
-  view,
-}) => (
-  <button
-    className={mergeClassNames(
+export default class Tile extends Component {
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const {
+      date,
+      tileClassName,
+      tileContent,
+      view,
+    } = nextProps;
+
+    const nextState = {};
+
+    if (tileClassName !== prevState.tileClassNameProps) {
+      nextState.tileClassName = typeof tileClassName === 'function' ? tileClassName({ date, view }) : tileClassName;
+      nextState.tileClassNameProps = tileClassName;
+    }
+
+    if (tileContent !== prevState.tileContentProps) {
+      nextState.tileContent = typeof tileContent === 'function' ? tileContent({ date, view }) : tileContent;
+      nextState.tileContentProps = tileContent;
+    }
+
+    return nextState;
+  }
+
+  state = {};
+
+  render() {
+    const {
+      activeStartDate,
+      children,
       classes,
-      tileClassName instanceof Function ? tileClassName({ date, view }) : tileClassName,
-    )}
-    disabled={
-      (minDate && minDateTransform(minDate) > date)
-      || (maxDate && maxDateTransform(maxDate) < date)
-      || (tileDisabled && tileDisabled({ activeStartDate, date, view }))
-    }
-    onClick={onClick && (() => onClick(date))}
-    onMouseOver={onMouseOver && (() => onMouseOver(date))}
-    onFocus={onMouseOver && (() => onMouseOver(date))}
-    style={style}
-    type="button"
-  >
-    {formatAbbr
-      ? (
-        <abbr aria-label={formatAbbr(date)}>
-          {children}
-        </abbr>
-      )
-      : children
-    }
-    {typeof tileContent === 'function' ? tileContent({ date, view }) : tileContent}
-  </button>
-);
+      date,
+      formatAbbr,
+      maxDate,
+      maxDateTransform,
+      minDate,
+      minDateTransform,
+      onClick,
+      onMouseOver,
+      style,
+      tileDisabled,
+      view,
+    } = this.props;
+    const { tileClassName, tileContent } = this.state;
+
+    return (
+      <button
+        className={mergeClassNames(classes, tileClassName)}
+        disabled={
+          (minDate && minDateTransform(minDate) > date)
+          || (maxDate && maxDateTransform(maxDate) < date)
+          || (tileDisabled && tileDisabled({ activeStartDate, date, view }))
+        }
+        onClick={onClick && (() => onClick(date))}
+        onMouseOver={onMouseOver && (() => onMouseOver(date))}
+        onFocus={onMouseOver && (() => onMouseOver(date))}
+        style={style}
+        type="button"
+      >
+        {formatAbbr
+          ? (
+            <abbr aria-label={formatAbbr(date)}>
+              {children}
+            </abbr>
+          )
+          : children
+        }
+        {tileContent}
+      </button>
+    );
+  }
+}
 
 Tile.propTypes = {
   ...tileProps,
@@ -57,5 +84,3 @@ Tile.propTypes = {
   maxDateTransform: PropTypes.func.isRequired,
   minDateTransform: PropTypes.func.isRequired,
 };
-
-export default Tile;
