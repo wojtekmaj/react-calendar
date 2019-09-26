337b1f8efe11810a6c935d48d173ebba95ea2995
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-09-26 15:03:13 +0200

    Create ESM builds

--- copy-styles.js
+++ copy-styles.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 
-fs.copyFile('src/Calendar.less', 'dist/Calendar.less', (error) => {
+fs.copyFile('src/Calendar.less', 'dist/esm/Calendar.less', (error) => {
   if (error) {
     throw error;
   }
   // eslint-disable-next-line no-console
-  console.log('Calendar.less copied successfully.');
+  console.log('Calendar.less copied successfully to ESM build.');
+});
+
+fs.copyFile('src/Calendar.less', 'dist/umd/Calendar.less', (error) => {
+  if (error) {
+    throw error;
+  }
+  // eslint-disable-next-line no-console
+  console.log('Calendar.less copied successfully to UMD build.');
 });
