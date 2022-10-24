a8ea9222bdeed15e2e68427209c1ae782844c438
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-10-24 17:38:17 +0200

    Update React to 18.0.0

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   resolve: {
     alias: {
       'react': require.resolve('react'),
+      'react-dom/client': require.resolve('react-dom/client'),
       'react-dom': require.resolve('react-dom'),
     },
     extensions: ['.js', '.jsx'],
