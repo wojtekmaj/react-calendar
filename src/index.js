212c97d04041d7409e4504cf424f4b1fdb2bea82
Alex Goncharov <alex@yolkstudio.com>
2022-12-14 18:42:52 +0100

    exposed shared to a public package API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import CenturyView from './CenturyView';
 import DecadeView from './DecadeView';
 import YearView from './YearView';
 import MonthView from './MonthView';
+import * as Shared from './shared';
 
 export default Calendar;
 
-export { Calendar, Navigation, CenturyView, DecadeView, YearView, MonthView };
+export { Calendar, Navigation, CenturyView, DecadeView, YearView, MonthView, Shared };
---
+++ src/shared/index.js
@@ -0,0 +1,4 @@
+export * as Const from './const';
+export * as DateFormatter from './dateFormatter';
+export * as Dates from './dates';
+export * as Utils from './utils';
