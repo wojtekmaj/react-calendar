a45a720c0976a48e54ca17b8db5c3204392512ae
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-05-09 01:15:20 +0200

    Improve types

--- src/shared/propTypes.ts
+++ src/shared/propTypes.ts
@@ -62,7 +62,7 @@ export function isMaxDate(props: Record<string, unknown>, propName: string, comp
 
 export const isRef = PropTypes.oneOfType([
   PropTypes.func,
-  PropTypes.shape({
+  PropTypes.exact({
     current: PropTypes.any,
   }),
 ]);
