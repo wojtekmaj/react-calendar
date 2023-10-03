3e76f046396e3fd1fa1d26245eb7c94671fd76b4
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-10-03 11:08:46 +0200

    Replace JSDom with Happy DOM

--- packages/react-calendar/vitest.config.ts
+++ packages/react-calendar/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: 'vitest.setup.ts',
     watch: false,
   },
