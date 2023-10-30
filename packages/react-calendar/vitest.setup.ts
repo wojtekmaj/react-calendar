16c2fe88d4bb9d1bf70107a4fddf67ac9e8330b8
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-10-30 21:36:25 +0100

    Update @testing-library/jest-dom to 6.0.0

--- packages/react-calendar/vitest.setup.ts
+++ packages/react-calendar/vitest.setup.ts
@@ -1,8 +1,6 @@
-import { expect, afterEach } from 'vitest';
+import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-import matchers from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers);
+import '@testing-library/jest-dom/vitest';
 
 afterEach(() => {
   cleanup();
