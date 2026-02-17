cedb59c8d551af06a1ce67fdc7677beebacbeea7
Wojciech Maj <kontakt@wojtekmaj.pl>
2026-02-17 13:21:13 +0100

    Update React to 19.2.0

--- packages/react-calendar/src/Calendar.spec.tsx
+++ packages/react-calendar/src/Calendar.spec.tsx
@@ -1,8 +1,7 @@
 import { describe, expect, it, vi } from 'vitest';
 import { userEvent } from 'vitest/browser';
 import { render } from 'vitest-browser-react';
-import { createRef } from 'react';
-import { act } from 'react-dom/test-utils';
+import { act, createRef } from 'react';
 import { getMonthStart } from '@wojtekmaj/date-utils';
 
 import Calendar from './Calendar.js';
