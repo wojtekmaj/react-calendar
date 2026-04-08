3505b150e12ead0ad2f8bf029d6ae293d838bdf2
Wojciech Maj <kontakt@wojtekmaj.pl>
2026-04-08 11:29:37 +0200

    Remove unnecessary biome-ignore

--- test/MinDetailOptions.tsx
+++ test/MinDetailOptions.tsx
@@ -40,7 +40,6 @@ export default function MinDetailOptions({
             type="radio"
             value={view}
           />
-          {/* biome-ignore lint/a11y/noLabelWithoutControl: Pinky promise this label won't ever be empty */}
           <label htmlFor={`min-${view}`}>{upperCaseFirstLetter(view)}</label>
         </div>
       ))}
